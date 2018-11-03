9a7bb64d4802ab5c3cd72bcbadee59e43fbbc2b8
sifan <718662552@qq.com>
2018-11-03 16:09:10 +0800

    优化目录结构

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="home">
     <p class="unit">{{msg}}</p>
     <p>vuex 属性值：{{test}}</p>
     <jsx-test></jsx-test>
@@ -7,9 +7,9 @@
 </template>
 
 <script>
-import jsxTest from "./jsx";
+import jsxTest from "@/components/jsx";
 export default {
-  name: "HelloWorld",
+  name: "Home",
   data() {
     return {
       msg: "unit",
@@ -22,20 +22,22 @@ export default {
 };
 </script>
 
-<style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style scoped lang="scss">
+.home {
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
 }
 </style>
